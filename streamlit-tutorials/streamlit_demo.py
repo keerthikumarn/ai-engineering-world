3270981ee61823353be32d9b707644df9908c82b
Keerthi Kumar <n.keerthikumar85@gmail.com>
2026-02-02 22:21:13 +0530

    charts and plots

---
+++ streamlit-tutorials/streamlit_demo.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+import altair as alt
+
+# Basic of Stremlit
+
+st.title("Welcome to the world of streamlit programming")
+st.header("This is a header section")
+st.subheader("This is a sub header section")
+st.text("This is a sample text")
+
+# Working with datasets
+a = [1,2,3,4,5,6,7,8]
+b = [11,12,13,14,15,16,17,18]
+n = np.array(a)
+nd = n.reshape((2,4))
+
+# Sample dictionary
+dic = {
+    "name": "Keerthi Kumar N",
+    "age": 40,
+    "city": "India"
+}
+
+data = pd.read_csv("stock.csv")
+df = pd.DataFrame(np.random.randn(100,3), columns=['a','b','c'])
+
+st.dataframe(a);
+st.json(dic)
+st.write(nd)
+
+# Charts and plots
+st.line_chart(df)
+st.bar_chart(df)
+st.area_chart(df)
